916ab70b29129cb8dcb7e51c39b9b683392fa882
John Bermeo <john.bermeo@gmail.com>
2024-03-25 17:02:47 +0100

    sw-02: folder structure and basic route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./core/components/main-layout/main-layout.component').then(m => m.MainLayoutComponent)
+  }
+];
---
+++ src/app/modules/people/components/people-details/people-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './people-details.component.html',
+  styleUrl: './people-details.component.scss'
+})
+export class PeopleDetailsComponent {
+
+}
